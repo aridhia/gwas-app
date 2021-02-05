67ef2700a68622de73925d78500f9ffda43259af
annadomenech <anna.domenech@aridhia.com>
2021-02-05 11:29:48 +0000

    fixed help tab

--- code/help_tab.R
+++ code/help_tab.R
@@ -4,33 +4,26 @@ documentation_tab <- function() {
                      fluidRow(column(
                        6,
                        h3("Genome Wide Association Studies (GWAS)"), 
-                       p("This mini-app allows you to easily visualise the results from a GwAS. GWAS is a hypothesis-free mothod for identifying associations between genetic regions and traits
+                       p("This RShiny mini-app allows you to easily visualise the results from GWAS, which is a hypothesis-free mothod for identifying associations between genetic regions and traits
                          (e.g. diseases). This kind of studies search for ", strong("Single-Nucleotide Polymorphisms (SNPs)"), " , which are small variations in the genetic 
                          code. Through GWAS, it is possible to identify SNPs that occur more frequently in certain diseases"),
-                       p("GWAS use two groups of participants, cases and controls. If certain SNPs are found to be significantly more frequent in cases of a certain disease
-                         compared to controls, the SNPs could be associated with the disease or trait."),
-                       p("GWAS results are displayed in a ", strong("Manhattan plot"), "which shows the -log10(p-value) against the position in the genome. The signficace
+                       p("The results are displayed in a ", strong("Manhattan plot"), "which shows the -log10(p-value) against the position in the genome. The signficace
                          threshold for this studies is p = 5 x 10", tags$sup("-8.")),
-                       h4("Mini-app layout"),
-                       p("The mini-app contains 4 tabs; this help tab gives you an overview of the mini-app itself."),
-                       
-                       h5("To use the mini-app"),
+                       h4("How use the mini-app"),
                        p("The data being used by the app is the HapMap dataset from the manhattanly R package, to use a different dataset you should store the 
                          csv file in the data folder" ),
                        
                        tags$ol(
-                         tags$li("The first tab displays an ", strong("interactive Manhattan plot"), " showing the -log10(p-value) against the position in the genome.
-                                 The red line shows the significace threshold and the blue line shows the suggestive line. 
-                                 You can hover the moues over the SNPs to display the variant information. Using the options on the left-side bar, the user can change
+                         tags$li("The first tab displays an ", strong("interactive Manhattan plot"), ", the red line shows the significace threshold and the blue line shows the suggestive line. 
+                                 Hovering the moues over the SNPs will display the variant information. Using the options on the left-side bar, the user can change
                                  the color scheme, choose to display one or multiple chromosomes, select the size of the points and remove the suggestive ans siginificance
-                                 lines. A menu will appear in the upper-right corner of the graph, this allows to zoom in on a region of interest and export the image 
-                                 as a .png file"), 
+                                 lines."), 
                          
                          tags$li("The second tab prints a ", strong("Circular Manhattan plot."), 
-                                 "This plot is not interactive, but it is possible to change the color scheme using the options on the left."),
+                                 "This plot is also interactive, you can change the color scheme using the options on the left."),
                          tags$li("In the third tab builds a ", strong("Quantile-quantile (QQ) plot, "), 
-                                 "which are probability plots that are used to compare two probability distributions by plotting their quantiles against each other. In GWAS
-                                 studies, the QQ plot is a graphical representation of the deviation of the observed p-value against the expected p-values from a 
+                                 "which are probability plots used to compare two probability distributions by plotting their quantiles against each other. In GWAS
+                                 , the QQ plot is a graphical representation of the deviation of the observed p-value against the expected p-values from a 
                                  theoretical distribution"), 
                          tags$li("Finally, the fourth tab build the ", strong("SNP density plot, "), "which shows the number of candidate variants in consecutive 100-kb regions")
                        )
